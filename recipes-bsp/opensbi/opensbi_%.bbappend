riscv/meta-riscv

commit af1065b355ad4278f6a1826edb8c81b91c660a3e

opensbi-revyos: add recipe
opensbi-revyos is an opensbi fork, specific for beaglev-ahead MACHINE.

this patch was originally authored by Gyorgy Sarvari, but as per the
discussion on the mailing list[1], I am taking it up and filing the PR.

[1] https://lists.yoctoproject.org/g/yocto/message/65466

Signed-off-by: Gyorgy Sarvari <skandigraun@gmail.com>
Signed-off-by: Kanak Shilledar <kanakshilledar111@protonmail.com>

recipes-bsp/opensbi/opensbi_%.bbappend
@@ -6,9 +6,6 @@ SRCREV:ae350-ax45mp = "22f38ee6c658a660083aa45c4ec6c72f66a17260"
 SRCREV:jh7100 = "c6a092cd80112529cb2e92e180767ff5341b22a3"
 SRCREV:star64 = "c6a092cd80112529cb2e92e180767ff5341b22a3"
 SRCREV:milkv-duo = "v1.4"
-# recent version after 1.4 release
-SRCREV:c910 = "61d7484c752a5e4c464d5dc18e21d9ac67fbbefa"
-SRCREV:orangepi-rv2 = "89bff4a7e4cadfb5f130edb1ec44c39bff20a427"
 
 SRC_URI:star64 = "git://github.com/starfive-tech/opensbi;branch=JH7110_VisionFive2_devel;protocol=https"
 SRC_URI:append:star64 = "\
@@ -16,10 +13,6 @@ SRC_URI:append:star64 = "\
 	"
 
 SRC_URI:milkv-duo = "git://github.com/riscv-software-src/opensbi.git;branch=master;protocol=https"
-SRC_URI:c910 = "git://github.com/revyos/thead-opensbi.git;branch=th1520;protocol=https"
-SRC_URI:orangepi-rv2 = "git://github.com/orangepi-xunlong/u-boot-orangepi.git;protocol=https;branch=${BRANCH}"
-SRC_URI:append:orangepi-rv2 = " file://c23-compatibility.patch"
-S:orangepi-rv2 = "${UNPACKDIR}/${BP}/opensbi"
 
 DEPENDS:append:jh7110 = " u-boot-tools-native dtc-native"
 DEPENDS:append:orangepi-rv2 = " u-boot-tools-native"

conf/machine/beaglev-ahead.conf
@@ -35,5 +35,6 @@ WKS_FILE ?= "beaglev-ahead.wks"
 # Common Preferred Providers
 PREFERRED_PROVIDER_virtual/kernel ?= "linux-beaglev-dev"
 PREFERRED_PROVIDER_virtual/bootloader ?= "u-boot-beaglev-ahead"
+PREFERRED_PROVIDER_opensbi ?= "opensbi-revyos"
 EXTRA_IMAGEDEPENDS += "u-boot-beaglev-ahead"
 #============================================

recipes-bsp/opensbi/opensbi-revyos/0001-add-missing-LDFLAGS-to-Makefile.patch
@@ -1,0 +1,40 @@
+From e95af3b36b6194f51b485316ad0fefa7c60be4e0 Mon Sep 17 00:00:00 2001
+From: Gyorgy Sarvari <skandigraun@gmail.com>
+Date: Thu, 5 Jun 2025 08:48:41 +0200
+Subject: [PATCH] add missing LDFLAGS to Makefile
+
+CLFAGS and ASFLAGS are not extended by the Makefile, rather they are
+constructed from scratch, omitting some of the important Yocto-specific
+flags (e.g. file-prefix-map) which are causing fatal QA errors.
+
+Pass these flags explicitly in the Makefile.
+
+Upstream-Status: Pending
+
+Signed-off-by: Gyorgy Sarvari <skandigraun@gmail.com>
+---
+ Makefile | 4 ++--
+ 1 file changed, 2 insertions(+), 2 deletions(-)
+
+diff --git a/Makefile b/Makefile
+index 9ad599b..88c736e 100644
+--- a/Makefile
++++ b/Makefile
+@@ -210,7 +210,7 @@ GENFLAGS	+=	$(libsbiutils-genflags-y)
+ GENFLAGS	+=	$(platform-genflags-y)
+ GENFLAGS	+=	$(firmware-genflags-y)
+ 
+-CFLAGS		=	-g -Wall -Werror -ffreestanding -nostdlib -fno-stack-protector -fno-strict-aliasing -O2
++CFLAGS		=	-g -Wall -Werror -ffreestanding -nostdlib -fno-stack-protector -fno-strict-aliasing -O2 $(LDFLAGS)
+ CFLAGS		+=	-fno-omit-frame-pointer -fno-optimize-sibling-calls
+ CFLAGS		+=	-mno-save-restore -mstrict-align
+ CFLAGS		+=	-mabi=$(PLATFORM_RISCV_ABI) -march=$(PLATFORM_RISCV_ISA)
+@@ -224,7 +224,7 @@ CPPFLAGS	+=	$(GENFLAGS)
+ CPPFLAGS	+=	$(platform-cppflags-y)
+ CPPFLAGS	+=	$(firmware-cppflags-y)
+ 
+-ASFLAGS		=	-g -Wall -nostdlib
++ASFLAGS		=	-g -Wall -nostdlib $(LDFLAGS)
+ ASFLAGS		+=	-fno-omit-frame-pointer -fno-optimize-sibling-calls
+ ASFLAGS		+=	-mno-save-restore -mstrict-align
+ ASFLAGS		+=	-mabi=$(PLATFORM_RISCV_ABI) -march=$(PLATFORM_RISCV_ISA)

recipes-bsp/opensbi/opensbi-revyos/0001-c23-compatibility.patch
@@ -1,0 +1,50 @@
+From c315f807c0ee23f84ba481c48447e9c6f5a2b6cb Mon Sep 17 00:00:00 2001
+From: Gyorgy Sarvari <skandigraun@gmail.com>
+Date: Wed, 4 Jun 2025 17:28:19 +0200
+Subject: [PATCH]  fix c23 compatibility
+
+Upstream-Status: Backport [https://github.com/riscv-software-src/opensbi/commit/d539d64a23bbd64ba324eed595449bcc9c7be0cb]
+---
+ include/sbi/sbi_types.h | 9 ++++++---
+ 1 file changed, 6 insertions(+), 3 deletions(-)
+
+diff --git a/include/sbi/sbi_types.h b/include/sbi/sbi_types.h
+index 38e3565..058ee9d 100644
+--- a/include/sbi/sbi_types.h
++++ b/include/sbi/sbi_types.h
+@@ -44,7 +44,12 @@ typedef unsigned long long	uint64_t;
+ #error "Unexpected __riscv_xlen"
+ #endif
+ 
+-typedef int			bool;
++#if __STDC_VERSION__ < 202000L
++typedef _Bool			bool;
++#define true                    1
++#define false                   0
++#endif
++
+ typedef unsigned long		ulong;
+ typedef unsigned long		uintptr_t;
+ typedef unsigned long		size_t;
+@@ -56,8 +61,6 @@ typedef unsigned long		physical_size_t;
+ 
+ #define TRUE			1
+ #define FALSE			0
+-#define true			TRUE
+-#define false			FALSE
+ 
+ #define NULL			((void *)0)
+ 
+diff --git a/lib/sbi/riscv_locks.c b/lib/sbi/riscv_locks.c
+index 75b443b..784530d 100644
+--- a/lib/sbi/riscv_locks.c
++++ b/lib/sbi/riscv_locks.c
+@@ -13,7 +13,7 @@ static inline int spin_lock_unlocked(spinlock_t lock)
+ 	return lock.owner == lock.next;
+ }
+ 
+-bool spin_lock_check(spinlock_t *lock)
++int spin_lock_check(spinlock_t *lock)
+ {
+ 	RISCV_FENCE(r, rw);
+ 	return !spin_lock_unlocked(*lock);

recipes-bsp/opensbi/opensbi-revyos_git.bb
@@ -1,0 +1,54 @@
+SUMMARY = "RISC-V Open Source Supervisor Binary Interface (OpenSBI)"
+DESCRIPTION = "OpenSBI aims to provide an open-source and extensible implementation of the RISC-V SBI specification for a platform specific firmware (M-mode) and a general purpose OS, hypervisor or bootloader (S-mode or HS-mode). OpenSBI implementation can be easily extended by RISC-V platform or System-on-Chip vendors to fit a particular hadware configuration."
+HOMEPAGE = "https://github.com/riscv/opensbi"
+LICENSE = "BSD-2-Clause"
+LIC_FILES_CHKSUM = "file://COPYING.BSD;md5=42dd9555eb177f35150cf9aa240b61e5"
+
+
+PROVIDES += "opensbi"
+require recipes-bsp/opensbi/opensbi-payloads.inc
+
+inherit autotools-brokensep deploy
+
+SRCREV = "61d7484c752a5e4c464d5dc18e21d9ac67fbbefa"
+SRC_URI = "git://github.com/revyos/thead-opensbi.git;branch=th1520;protocol=https \
+           file://0001-c23-compatibility.patch \
+           file://0001-add-missing-LDFLAGS-to-Makefile.patch"
+
+#S = "${WORKDIR}/git"
+
+EXTRA_OEMAKE += "PLATFORM=${RISCV_SBI_PLAT} I=${D} FW_PIC=n CLANG_TARGET= "
+# If RISCV_SBI_PAYLOAD is set then include it as a payload
+EXTRA_OEMAKE:append = " ${@riscv_get_extra_oemake_image(d)}"
+EXTRA_OEMAKE:append = " ${@riscv_get_extra_oemake_fdt(d)}"
+
+do_configure[noexec] = "1"
+
+# Required if specifying a custom payload
+do_compile[depends] += "${@riscv_get_do_compile_depends(d)}"
+
+do_install:append() {
+	# In the future these might be required as a dependency for other packages.
+	# At the moment just delete them to avoid warnings
+	rm -r ${D}/include
+	rm -r ${D}/lib*
+	rm -r ${D}/share/opensbi/*/${RISCV_SBI_PLAT}/firmware/payloads
+}
+
+do_deploy () {
+	install -m 755 ${D}/share/opensbi/*/${RISCV_SBI_PLAT}/firmware/fw_payload.* ${DEPLOYDIR}/
+	install -m 755 ${D}/share/opensbi/*/${RISCV_SBI_PLAT}/firmware/fw_jump.* ${DEPLOYDIR}/
+	install -m 755 ${D}/share/opensbi/*/${RISCV_SBI_PLAT}/firmware/fw_dynamic.* ${DEPLOYDIR}/
+}
+
+addtask deploy before do_build after do_install
+
+FILES:${PN} += "/share/opensbi/*/${RISCV_SBI_PLAT}/firmware/fw_jump.*"
+FILES:${PN} += "/share/opensbi/*/${RISCV_SBI_PLAT}/firmware/fw_payload.*"
+FILES:${PN} += "/share/opensbi/*/${RISCV_SBI_PLAT}/firmware/fw_dynamic.*"
+
+COMPATIBLE_MACHINE = "beaglev-ahead"
+INHIBIT_PACKAGE_STRIP = "1"
+
+SECURITY_CFLAGS = ""
+

commit 2252725806009e042c7f1f72e4c05b68f51d32c1

opensbi: fix issues caused by #534
revert orangepi-rv2 patches removed by the PR#534

Signed-off-by: Kanak Shilledar <kanakshilledar111@protonmail.com>

recipes-bsp/opensbi/opensbi_%.bbappend
@@ -6,6 +6,7 @@ SRCREV:ae350-ax45mp = "22f38ee6c658a660083aa45c4ec6c72f66a17260"
 SRCREV:jh7100 = "c6a092cd80112529cb2e92e180767ff5341b22a3"
 SRCREV:star64 = "c6a092cd80112529cb2e92e180767ff5341b22a3"
 SRCREV:milkv-duo = "v1.4"
+SRCREV:orangepi-rv2 = "89bff4a7e4cadfb5f130edb1ec44c39bff20a427"
 
 SRC_URI:star64 = "git://github.com/starfive-tech/opensbi;branch=JH7110_VisionFive2_devel;protocol=https"
 SRC_URI:append:star64 = "\
@@ -13,6 +14,9 @@ SRC_URI:append:star64 = "\
 	"
 
 SRC_URI:milkv-duo = "git://github.com/riscv-software-src/opensbi.git;branch=master;protocol=https"
+SRC_URI:orangepi-rv2 = "git://github.com/orangepi-xunlong/u-boot-orangepi.git;protocol=https;branch=${BRANCH}"
+SRC_URI:append:orangepi-rv2 = " file://c23-compatibility.patch"
+S:orangepi-rv2 = "${UNPACKDIR}/${BP}/opensbi"
 
 DEPENDS:append:jh7110 = " u-boot-tools-native dtc-native"
 DEPENDS:append:orangepi-rv2 = " u-boot-tools-native"