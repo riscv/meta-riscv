riscv/meta-riscv

commit 7b28bb1902fa507c66848d81019965ccc9846cce

u-boot-spl-k1: new recipe
Builds only the K1 first stage bootloader containing initializations
that mainline U-Boot doesn't have yet, such as DRAM training.

Then mainline U-Boot can be used.

Signed-off-by: Michael Opdenacker <michael.opdenacker@rootcommit.com>

recipes-bsp/u-boot/u-boot-spl-k1.bb
@@ -1,0 +1,39 @@
+HOMEPAGE = "https://gitee.com/bianbu-linux/uboot-2022.10"
+DESCRIPTION = "U-Boot SPL for SpacemiT K1 SoCs, derived from U-Boot"
+SECTION = "bootloaders"
+DEPENDS += "flex-native bison-native python3-setuptools-native"
+LICENSE = "GPL-2.0-or-later"
+LIC_FILES_CHKSUM = "file://Licenses/README;md5=2ca5f2c35c8cc335f0a19756634782f1"
+
+inherit deploy
+
+SRC_URI = "git://gitee.com/bianbu-linux/uboot-2022.10;protocol=https;branch=k1-bl-v2.2.y"
+SRCREV = "c6f2746cb7993a6fb6c9f51b2bff318921e13f98"
+
+UBOOT_SPL_MACHINE ??= "k1_defconfig"
+
+EXTRA_OEMAKE = "\
+    CROSS_COMPILE=${TARGET_PREFIX} \
+    ARCH=riscv \
+    HOSTCC='${BUILD_CC}' \
+    HOSTCXX='${BUILD_CXX}' \
+"
+
+do_configure() {
+	oe_runmake ${UBOOT_SPL_MACHINE}
+}
+
+do_compile() {
+    LIBGCC_PATH=$(${CC} ${CFLAGS} ${LDFLAGS} -print-libgcc-file-name)
+    oe_runmake PLATFORM_LIBGCC="${LIBGCC_PATH}" spl/u-boot-spl.bin
+}
+
+do_deploy() {
+	install -d ${DEPLOYDIR}
+	install -m 644 ${B}/FSBL.bin ${DEPLOYDIR}/
+	install -m 644 ${B}/spl/u-boot-spl.bin ${DEPLOYDIR}/
+	install -m 644 ${B}/bootinfo_emmc.bin ${DEPLOYDIR}/
+	install -m 644 ${B}/bootinfo_sd.bin ${DEPLOYDIR}/
+}
+
+addtask deploy before do_build after do_compile

commit 45cb58019b3515eccf265f8950188f3fd31b7346

u-boot-spl-k1: fix openssl/evp.h include not found
This fixes the following build error:
  HOSTCC  tools/image-host.o
  WRAP    tools/boot/image-host.c
tools/Makefile:271: warning: pattern recipe did not update peer target 'tools/lib/image-host.c'.
tools/Makefile:271: warning: pattern recipe did not update peer target 'tools/env/image-host.c'.
tools/Makefile:271: warning: pattern recipe did not update peer target 'tools/common/image-host.c'.
In file included from tools/imagetool.h:24,
                 from tools/mkimage.h:22,
                 from tools/image-host.c:11:
include/image.h:1254:12: fatal error: openssl/evp.h: No such file or directory
 1254 | #  include <openssl/evp.h>
      |            ^~~~~~~~~~~~~~~
compilation terminated.

Fixes: 7b28bb1902fa ("u-boot-spl-k1: new recipe")

Signed-off-by: Marcel Ziswiler <marcel@ziswiler.com>

recipes-bsp/u-boot/u-boot-spl-k1.bb
@@ -15,7 +15,7 @@ UBOOT_SPL_MACHINE ??= "k1_defconfig"
 EXTRA_OEMAKE = "\
     CROSS_COMPILE=${TARGET_PREFIX} \
     ARCH=riscv \
-    HOSTCC='${BUILD_CC}' \
+    HOSTCC='${BUILD_CC} ${BUILD_CFLAGS} ${BUILD_LDFLAGS}' \
     HOSTCXX='${BUILD_CXX}' \
 "
 